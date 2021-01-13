9e5be321cdac6e974dfdc377265075080998514a
Ibad <ibadsaladdin@gmail.com>
2021-01-13 22:40:16 +0700

    react mapping

--- react-props/src/components/App.js
+++ react-props/src/components/App.js
@@ -3,29 +3,24 @@ import Card from "./Card";
 import Avatar from "./Avatar";
 import contacts from "../contacts";
 
+function mapContact(contact){
+  return(
+    <Card
+      key = {contact.id}
+      name = {contact.name}
+      imageLink = {contact.imgURL}
+      phone = {contact.phone}
+      email = {contact.email}
+    />
+  );
+}
+
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
       <Avatar imageLink = "https://www.ibad.me/images/logo2.png" />
-      <Card 
-          name = {contacts[0].name}
-          imageLink = {contacts[0].imgURL}
-          phone = {contacts[0].phone}
-          email = {contacts[0].email}
-      />
-      <Card 
-          name = {contacts[1].name}
-          imageLink = {contacts[1].imgURL}
-          phone = {contacts[1].phone}
-          email = {contacts[1].email}
-      />
-      <Card 
-          name = {contacts[2].name}
-          imageLink = {contacts[2].imgURL}
-          phone = {contacts[2].phone}
-          email = {contacts[2].email}
-      />
+      {contacts.map(mapContact)}
     </div>
   );
 }
